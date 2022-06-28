9a967299c3f917cb7a85ba8cc2c812fb534d8ad7
AndreLuizPires <andreldsantosp@gmail.com>
2022-06-28 15:41:26 -0300

    Inclusão de itens vazios no menu

--- navbar.php
+++ navbar.php
@@ -2,6 +2,12 @@
         <ul>
             <li><a href="index.php">HOME</a></li>
             <li><a href="guerra.php">GUERRA</a></li>
+            <li><a href="#">DEFESAS</a></li>
+            <li><a href="#">DECKS GUERRA</a></li>
+            <li><a href="#">DECKS PVP</a></li>
+            <li><a href="#">DECKS COOP</a></li>
+            <li><a href="#">FOICE</a></li>
             <li><a href="#">SOBRE</a></li>
+            <li><a href="#">ADMIN</a></li>
         </ul>
     </nav>
